5e9e1c368d19de84ed7e6b2b63125f20798a6f0f
stefanopriolo <stefanojpriolo@gmail.com>
2023-08-21 18:40:43 +0200

    esercizio

---
+++ vue-vite/src/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'hello',
+    data() {
+        
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+    </template>
